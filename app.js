eb307bb87aacbd6d57e5c266ee30bfcf63344f21
wawerusamuel <waweruzamuel@gmail.com>
2022-09-28 23:56:57 +0300

    no changes made...

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const displayDiv = document.querySelector(".display h4");
   const date = new Date();
   displayDiv.innerText = date;
 })();
+
+const number = 9;
+const string = String(number);
+
+console.log(isNaN("DFDSF"));
+console.log(typeof string);
